e99455f821f2cc996e64c57bcd904e8376168931
Saeed <109621222+saeedvdi@users.noreply.github.com>
2024-11-15 12:06:39 -0500

    Update GolemConditionalFunctionEnableControl.C

--- src/controls/GolemConditionalFunctionEnableControl.C
+++ src/controls/GolemConditionalFunctionEnableControl.C
@@ -18,7 +18,7 @@
 /*    along with this program.  If not, see <http://www.gnu.org/licenses/>    */
 /******************************************************************************/
 
-#include "ConditionalFunctionEnableControl.h"
+#include "GolemConditionalFunctionEnableControl.h"
 #include "Function.h"
 
 registerMooseObject("GolemApp", ConditionalFunctionEnableControl);
